2b70df5e3fd62bb1844b44c5f27b4e7ad952be55
조현창 <gusckd57@gmail.com>
2023-01-13 18:44:29 +0900

    오답노트

--- src/domain/payments/payments.module.ts
+++ src/domain/payments/payments.module.ts
@@ -2,9 +2,15 @@ import { CacheModule, Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { PaymentsRepository } from './payments.repository';
+// 오답노트 import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+// 오답노트 CacheModule.register<ClientOpts>({
+//       store: redisStore,
+//       host: 'localhost',
+//       port: 6379,
+//     }),
     CacheModule.register({
       isGlobal: true,
     }),
